6d1069e10f3885cf06207294e9b6c0ded8a48e14
ployploy <chonnikan.t@kkumail.com>
2018-07-30 15:44:54 +0700

    [Edited]: แก้ไขข้อมูลที่จะใชในการทดสอบฟังก์ชั่นการทำงานโดยการเอาข้อมูลที่ mock ไว้มาใช้

--- src/service/topup_test.go
+++ src/service/topup_test.go
@@ -1,19 +1,23 @@
 package service
 
 import (
+	"dac"
 	"dto"
 	"testing"
 )
 
-func Test_Topup_Input_AccountNumberDestination_8976789543_Amount_5000_Should_Be_Ployploy_BalanceOld_15000_CurrentBalance_10000_Amount_5000(t *testing.T) {
+func Test_Topup_Input_AccountNumberDestination_8976789543_Amount_5000_Should_Be_Ployploy_BalanceOld_15000_CurrentBalance_20000_Amount_5000(t *testing.T) {
 	expected := dto.TopupResponse{
 		AccountName:    "Ployploy",
 		BalanceOld:     15000.00,
-		CurrentBalance: 10000.00,
+		CurrentBalance: 20000.00,
 		Topup:          5000.00,
 	}
+	accountDac := dac.NewAccountDacFake()
+	historyDac := dac.NewHistoryDacFake()
+	paymentSvc := NewPaymentSvc(accountDac, historyDac)
 
-	accualResult := Topup("8976789543", 5000)
+	accualResult := paymentSvc.Topup("8976789543", 5000.00)
 
 	if expected != accualResult {
 		t.Errorf("expected %v but got %v", expected, accualResult)
