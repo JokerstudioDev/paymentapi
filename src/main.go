15522bdf6f6e14ed205a9546bb1efd9f11dabfc4
ployploy <chonnikan.t@kkumail.com>
2018-08-12 15:18:41 +0700

    [Edited]: เรียกใช้งานฟังก์ชั่น newAccountDac

--- src/main.go
+++ src/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"dac"
+	"fmt"
+	"service"
+)
 
+func main() {
+	accountDac := dac.NewAccountDac("root:123456@tcp(127.0.0.1:3306)/workshop")
+	historyDac := dac.NewHistoryDacFake()
+	paymentSvc := service.NewPaymentSvc(accountDac, historyDac)
+	results := paymentSvc.Topup("ploy01", 5000.00)
+	fmt.Println(results)
 }
